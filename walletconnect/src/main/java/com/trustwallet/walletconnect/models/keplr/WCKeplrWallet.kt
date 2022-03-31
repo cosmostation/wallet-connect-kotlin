1072d7c55b42ff3308ade1b3b458f41bc6ea3d00
soaryong-c <soaryong@stamper.network>
2022-03-31 12:02:27 +0900

    [ANDWL-162] wc support keplr

---
+++ walletconnect/src/main/java/com/trustwallet/walletconnect/models/keplr/WCKeplrWallet.kt
@@ -0,0 +1,10 @@
+package com.trustwallet.walletconnect.models.keplr
+
+data class WCKeplrWallet(
+    val name: String,
+    val algo: String,
+    val pubKey: String,
+    val address: String,
+    val bech32Address: String,
+    val isNanoLedger: Boolean
+)
